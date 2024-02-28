0f94a8b6afc6237b8b620246eda179f68598ab77
cbndev99 <chandev99@hotmail.com>
2024-02-28 23:25:27 +0800

    secondcommit

--- app/(models)/Ticket.js
+++ app/(models)/Ticket.js
@@ -1,6 +1,6 @@
 import mongoose, { Schema } from "mongoose";
 
-mongoose.connect(process.env.MONGODB_URI2);
+mongoose.connect(process.env.MONGODB_URI);
 mongoose.Promise = global.Promise;
 
 const ticketSchema = new Schema(
--- app/page.js
+++ app/page.js
@@ -1,14 +1,48 @@
 import TicketCard from "./(components)/TicketCard";
 
-const Dashboard = () => {
+// get tickets
+const getTickets = async () => {
+  try {
+    const res = await fetch("http://localhost:3000/api/Tickets", {
+      method: "GET",
+      cache: "no-store",
+    });
+
+    return res.json();
+  } catch (error) {
+    console.log("Fail to get tickets", error);
+  }
+};
+
+const Dashboard = async () => {
+  const { tickets } = await getTickets();
+
+  // get categories
+  const uniqueCategories = [
+    // Set => take all the results and remove duplicate
+    ...new Set(tickets?.map(({ category }) => category)),
+  ];
+
   return (
     <div className="p-5">
-      <div className="lg:grid grid-cols-2 xl:grid-cols-4">
-        <TicketCard />
-        <TicketCard />
-        <TicketCard />
-        <TicketCard />
-        <TicketCard />
+      <div>
+        {tickets &&
+          uniqueCategories?.map((uniqueCategory, categoryIndex) => (
+            <div key={categoryIndex} className="mb-4">
+              <h2>{uniqueCategory}</h2>
+              <div className="lg:grid grid-cols-2 xl:grid-cols-4">
+                {tickets
+                  .filter((ticket) => ticket.category === uniqueCategory)
+                  .map((filteredTicket, _index) => (
+                    <TicketCard
+                      id={_index}
+                      key={_index}
+                      ticket={filteredTicket}
+                    />
+                  ))}
+              </div>
+            </div>
+          ))}
       </div>
     </div>
   );
